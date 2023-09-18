68337e11c2581dcf94d99ffcb8ca1f3a36db8460
igors <igor.sutikov@ivkhk.ee>
2023-09-18 12:58:25 +0300

    SCRIP-5 <Canvas JS>

---
+++ Canvas JS/game3.js
@@ -0,0 +1,62 @@
+let game = {
+    ctx: null,
+    platform: null, 
+    ball: null,
+    sprites: {
+        background: null,
+        ball: null,
+        platform: null
+    },
+    init: function () {
+        this.ctx = document.getElementById("mycanvas").getContext("2d");
+    },
+    preload(callback) {
+        let loaded = 0;
+        let required = Object.keys(this.sprites).length;
+
+        for (let key in this.sprites) {
+            this.sprites[key] = new Image();
+            this.sprites[key].src = "img/" + key + ".png";
+            this.sprites[key].addEventListener("load", () => {
+                ++loaded;
+                if (loaded >= required) {
+                    callback();
+                }
+            });
+        }
+    },
+    run() {
+        window.requestAnimationFrame(() => {
+            this.render();
+        });
+    },
+    render() {
+        this.ctx.drawImage(this.sprites.background, 0, 0);
+        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
+            this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+        this.ctx.drawImage(this.sprites.platform, 0, 0, this.platform.x, this.platform.y);
+    },
+    start: function() {
+        this.init();
+        this.preload(() => {
+            this.run();
+        });
+    }
+};
+
+game.ball = {
+    y: 280,
+    x: 320,
+    width: 20,
+    height: 20
+
+};
+
+game.platform = {
+    x:280,
+    y:320
+};
+
+window.addEventListener("load", () => {
+    game.start();
+});
